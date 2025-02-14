e16615cd81d7bed0634b9e45a4d23ef7d128b88a
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-02-14 16:26:50 +0600

    testing secure api call

---
+++ netlify/functions/apiCall.js
@@ -0,0 +1,29 @@
+exports.handler = async function (event, context) {
+    const errorText = event.queryStringParameters.errorText;
+    const apiKey = process.env.API_KEY;
+    fetch(
+    'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + apiKey,
+    {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        "contents": [{
+                "parts":[{"text": "Seperate the Main Messages of this error from The stack trace and write it & explain the error, Dont Specify how to fix it but explain the error given. Dont use any text styling like **, just add linebreaks to specify headings" + "The Error Is:- \n" + errorText}]
+                }]
+            })
+        }
+    ).then(res => res.json()).then(result => {
+        data = result;
+        message = data["candidates"][0]["content"]["parts"][0]["text"];
+        return {
+            statusCode: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*", // Adjust if needed
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ response: message }), 
+        };
+      })
+  };
